6527cd132e291ca95c96a67ef1217889851c0020
sexyloverman <83213661+sexyloverman@users.noreply.github.com>
2024-01-23 14:54:07 +0000

    initial commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // If we are on a local anvil, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_ANSWER = 2000e8;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    struct NetworkConfig {
+        address priceFeed; //Eth/USD price feed address
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        // price feed address
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory mainnetEthConfig = NetworkConfig(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+        // price feed address
+        return mainnetEthConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_ANSWER
+        );
+        vm.stopBroadcast();
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
